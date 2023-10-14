5804671b72251661a6664cd676d73a2e3a140cbd
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-10-14 16:19:28 +0530

    fix:numbers from transition

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import ParticlesContainer from '../components/ParticlesContainer'
+import ProjectsBtn from '../components/ProjectsBtn'
+import Avatar from '../components/Avatar'
+import { motion } from "framer-motion";
+import { fadeIn } from '../variants'
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+
+    </div>
+  )
 };
 
 export default Home;
