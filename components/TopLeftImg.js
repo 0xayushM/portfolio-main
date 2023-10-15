1bb2e2e576388399d42a8c35e9a28523dd3e67f6
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-10-15 10:17:04 +0530

    feat:completed about section

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -3,7 +3,7 @@ import LeftImage from '.././public/top-left-img.png'
 
 const TopLeftImg = () => {
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-1 w-[200px] xl:w-[400px] opacity-50">
       <Image src={LeftImage} width={400} height={400} alt=''/>
     </div>
   )
